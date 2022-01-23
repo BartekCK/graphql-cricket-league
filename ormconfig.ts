8632263d7a9a2aa2f0203731cfb070f2c04e84f0
Bartek <>
2022-01-23 18:11:22 +0100

    fix: files extension for db connect

--- ormconfig.ts
+++ ormconfig.ts
@@ -6,16 +6,13 @@ export = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: ["src/entities/*.ts"],
-  migrations: ["src/migrations/*.ts"],
-  // subscribers: ['**/*.subscriber{ .ts,.js }'],
+  entities: ["src/entities/*{.ts,.js}"],
+  migrations: ["src/migrations/*{.ts,.js}"],
   cli: {
     migrationsDir: "src/migrations",
     entitiesDir: "src/entities",
-    // subscribersDir: 'dist/**/**.subscriber{.ts,.js}',
   },
   synchronize: false,
-  // migrationsRun: true,
   logging: true,
   ssl: true,
   extra: {
